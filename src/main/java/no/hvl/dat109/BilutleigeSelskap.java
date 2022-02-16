f9d75de699b20b4c3993068a0808d41b5f4dfcf1
594620 <Gingerbread.Dragon@gmail.com>
2022-02-16 18:53:56 +0100

    Update BilutleigeSelskap.java

--- src/main/java/no/hvl/dat109/BilutleigeSelskap.java
+++ src/main/java/no/hvl/dat109/BilutleigeSelskap.java
@@ -1,11 +1,73 @@
 package no.hvl.dat109;
 
+import java.util.List;
+
 public class BilutleigeSelskap {
 
 	private String navn;
 	private int telefonnummer;
-	private String adresse;
+	private Adresse adresse;
+	private List<UtleigeKontor> utleigekontor;
+	private List<Kunde> kunder;
 	
-	
+	public BilutleigeSelskap(String navn, int telefonnummer, Adresse adresse, List<UtleigeKontor> utleigekontor,
+			List<Kunde> kunder) {
+		this.navn = navn;
+		this.telefonnummer = telefonnummer;
+		this.adresse = adresse;
+		this.utleigekontor = utleigekontor;
+		this.kunder = kunder;
+	}
 
+	
+	public void leggKontorTilListe(UtleigeKontor nyKontor) {
+		utleigekontor.add(nyKontor);
+	}
+	
+	public void leggTilKunde(Kunde k) {
+		kunder.add(k);
+	}
+	
+	
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+	public int getTelefonnummer() {
+		return telefonnummer;
+	}
+
+	public void setTelefonnummer(int telefonnummer) {
+		this.telefonnummer = telefonnummer;
+	}
+
+	public Adresse getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(Adresse adresse) {
+		this.adresse = adresse;
+	}
+
+	public List<UtleigeKontor> getUtleigekontor() {
+		return utleigekontor;
+	}
+
+	public void setUtleigekontor(List<UtleigeKontor> utleigekontor) {
+		this.utleigekontor = utleigekontor;
+	}
+
+	public List<Kunde> getKunde() {
+		return kunder;
+	}
+
+	public void setKunde(List<Kunde> kunde) {
+		this.kunder = kunde;
+	}
+	
+	
 }
